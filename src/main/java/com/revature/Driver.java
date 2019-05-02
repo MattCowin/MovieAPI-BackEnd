a93ddcbdb8d7c464bce210cd055afc61908b6a97
matt <mattcowin82@gmail.com>
2019-05-02 18:43:03 -0400

    not sure if there was any changes since last push

--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -14,7 +14,8 @@ public class Driver {
 	
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-		UserService userService = context.getBean("userService", UserService.class);
-		logger.trace(userService.registerUser(new User(0, "Cowin", "1234")));
-	}
+//		UserService userService = context.getBean("userService", UserService.class);
+//		logger.trace(userService.registerUser(new User(0, "Cowin", "1234")));
+//	
+		}
 }
