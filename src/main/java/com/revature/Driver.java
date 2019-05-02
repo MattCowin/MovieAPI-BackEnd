8ad156747a0ba0a331fd34f798c7dec98334cd3f
matt <mattcowin82@gmail.com>
2019-05-02 11:38:19 -0400

    removed the application context from src/main/resources

--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -4,11 +4,17 @@ import org.apache.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.revature.model.User;
+import com.revature.service.UserService;
+
 public class Driver {
 
 	private static Logger logger = Logger.getLogger(Driver.class);
 	
+	
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		UserService userService = context.getBean("userService", UserService.class);
+		logger.trace(userService.registerUser(new User(0, "Cowin", "1234")));
 	}
 }
