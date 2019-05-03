3c9e1415bf100c2f7c3364711cb11dd858397bc2
matt <mattcowin82@gmail.com>
2019-05-03 16:02:07 -0400

    Removed User Controller and project runs again

---
+++ src/main/java/com/revature/model/PopMovie.java
@@ -0,0 +1,75 @@
+package com.revature.model;
+
+import java.util.ArrayList;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class PopMovie {
+	
+	@Id
+	@Column(name="id")
+	private int popId;
+	@Column(name="title")
+	private String title;
+	@Column(name="genre_ids")
+	private ArrayList<PopMovie> genre = new ArrayList<>();
+	@Column(name="release_date")
+	private String releaseDate;
+	
+	public PopMovie() {
+		
+	}
+
+	public PopMovie(int popId, String title, ArrayList<PopMovie> genre, String releaseDate) {
+		this.popId = popId;
+		this.title = title;
+		this.genre = genre;
+		this.releaseDate = releaseDate;
+	}
+
+	public int getPopId() {
+		return popId;
+	}
+
+	public void setPopId(int popId) {
+		this.popId = popId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public ArrayList<PopMovie> getGenre() {
+		return genre;
+	}
+
+	public void setGenre(ArrayList<PopMovie> genre) {
+		this.genre = genre;
+	}
+
+	public String getReleaseDate() {
+		return releaseDate;
+	}
+
+	public void setReleaseDate(String releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+
+	@Override
+	public String toString() {
+		return "PopMovie [popId=" + popId + ", title=" + title + ", genre=" + genre + ", releaseDate=" + releaseDate
+				+ "]";
+	}
+	
+	
+	
+}
