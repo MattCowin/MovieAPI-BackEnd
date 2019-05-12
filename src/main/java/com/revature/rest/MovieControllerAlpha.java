723d2c3d0eb8e74c46f25fdefe754150b64e1499
mccollum3d <mccollum3d@gmail.com>
2019-05-11 21:50:09 -0400

    Cleaned up code. Tested: Deleted unused PopularMovies model. Deleted comments to clean up code, made new comments where applicable. Cleaned up after testing Angular build. Angular does not work because it is not yet configured correctly. Preparing Java code to be deployed to EC2.

--- src/main/java/com/revature/rest/MovieControllerAlpha.java
+++ src/main/java/com/revature/rest/MovieControllerAlpha.java
@@ -43,13 +43,11 @@ public class MovieControllerAlpha implements MovieController {
 	@Autowired
 	private UserService userService;
 	
-	@Override // GET  /findMovie?movieId=
-	@GetMapping(value="/findMovie", produces = MediaType.APPLICATION_JSON_VALUE)
-	public Movie findMovieById(@RequestParam("movieId") Integer movieId) {
-		logger.trace("*** Finding movie by ID " + movieId);
-		return movieService.getMovieById(movieId);
-	}
-	
+	/*
+	 * Gets the favorites of the user whose ID is supplied.
+	 * Accesses a List<Movie> of that user which is created through @ManyToMany 
+	 * and an @JoinTable in the Users.class
+	 */
 	@Override //GET   /myFavorites?userId=
 	@GetMapping(value="/myFavorites", produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Movie> findMyFavorites(@RequestParam("userId") Integer userId)
@@ -63,18 +61,28 @@ public class MovieControllerAlpha implements MovieController {
 
 	}
 	
+	/*
+	 * Gets recommendations based on the supplied movie ID.
+	 * The JSON returned contains a nested array "results", which must be extracted and passed
+	 * to Jackson to be marshaled. I did this by assigning the API's JSON response to a temp
+	 * object, and then calling the getResults() method to return the nested array.
+	 */
 	@Override  		  //Recommend?movieId=
 	@GetMapping(value="/Recommend", produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Movie> getRecommended(@RequestParam("movieId") Integer movieId) {
 		String url = MOVIEDB_MOVIE_URL + movieId + "/recommendations?api_key="
-				+ API_KEY + "&language=en-US&page=1";						//build the uri string
+				+ API_KEY + "&language=en-US&page=1";
 		logger.trace(" *** API Request URL Built: " + url);
-		RestTemplate restTemplate = new RestTemplate();						//new rest template
-		APIResponse myResponse = new APIResponse();							//new temp object to hold response in supplied format
-		myResponse = restTemplate.getForObject(url, APIResponse.class);		//get the API's info
-		return myResponse.getResults();										//get just the array of Movies from the temp object.
-	}																		//This solved the problem with nested JSON.
+		RestTemplate restTemplate = new RestTemplate();	
+		APIResponse myResponse = new APIResponse();	
+		myResponse = restTemplate.getForObject(url, APIResponse.class);	
+		return myResponse.getResults();	
+	}
 	
+	/*
+	 * By going to the popular endpoint, you will get a list of popular movies
+	 * as determined by the Movie API.
+	 */
 	@Override  		  //Popular
 	@GetMapping(value="/Popular", produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Movie> getPopular() {
@@ -87,21 +95,44 @@ public class MovieControllerAlpha implements MovieController {
 		return myResponse.getResults();										
 	}		
 	
-	@Override	// Method depreciated. Left in for testing purposes.
+	/*
+	 * Method Depreciated. Left in for testing.
+	 * Pulls a cached movie out of our Database by ID.
+	 */
+	@Override // GET  /findMovie?movieId=
+	@GetMapping(value="/findMovie", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Movie findMovieById(@RequestParam("movieId") Integer movieId) {
+		logger.trace("*** Finding movie by ID " + movieId);
+		return movieService.getMovieById(movieId);
+	}
+	
+	/*
+	 * Method Depreciated. Left in for testing.
+	 * Adds a movie to the database.
+	 */
+	@Override
 	@PostMapping("/favorite")
 	public ClientMessage addFavoriteMovie(@RequestBody Movie movie) {
 		logger.trace("*** Registering a new favorite movie " + movie);
 		return (movieService.registerMovie(movie)) ? MOVIE_FAV_SUCCESS : SOMETHING_WENT_WRONG;
 	}
 
-	@Override	// Method depreciated. Left in for testing purposes.
+	/*
+	 * Method Depreciated. Left in for testing.
+	 * Returns all movies from our database.
+	 */
+	@Override	
 	@GetMapping("/findAllMovies") 
 	public List<Movie> findAllMovies() {
 		logger.trace(" *** Getting List of all Favorite Movies. ");
 		return movieService.getAllMovies();
 	}
 
-	@Override	// Method depreciated. Left in for testing purposes.
+	/*
+	 * Method Depreciated. Left in for testing.
+	 * Send a title in JSON, get matching movie from Database.
+	 */
+	@Override
 	@PostMapping("/findMovie")
 	public Movie findMovie(@RequestBody Movie movie) {
 		logger.trace("*** Finding movie " + movie);
